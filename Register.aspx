a88e0180cd54cb9a14ca856e1df97e8061131a43
amrutgawade <amrutgawade.23@gmail.com>
2024-08-28 18:48:34 +0530

    Add project files.

---
+++ Register.aspx
@@ -0,0 +1,181 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="Employee_Registration.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Register</title>
+    <style>
+        body{
+            background-color: aliceblue;
+        }
+        #form1 {
+            border: 1px solid skyblue;
+            border-radius: 5px;
+            padding: 2rem;
+            width: 50%;
+            margin: 2rem auto;
+            background-color: white;
+        }
+
+        h1 {
+            margin: 2rem 0;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .inline-flex{
+            display:inline-flex;
+        }
+
+        .form-control {
+            display: flex;
+            justify-content: space-around;
+            width: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .form-group {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .input-text {
+            padding: 0.4rem 0.6rem;
+            border: 1px solid grey;
+            border-radius: 5px;
+            outline: none;
+            min-width: 15rem;
+        }
+
+            .input-text:hover, :focus {
+                border: 1px solid blue;
+            }
+
+        .form-control-left {
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+        }
+
+        .form-control-right {
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+        }
+
+        .btn-group {
+            display: flex;
+            gap: 20px;
+            margin-top: 1.2rem;
+            margin-left: 4rem;
+        }
+
+        .btn {
+            padding: 0.4rem 0.6rem;
+            outline: none;
+            border-radius: 5px;
+        }
+
+        .btn-primary {
+            background-color: blue;
+            border: 1px solid blue;
+            color: white;
+        }
+
+            .btn-primary:hover {
+                background-color: mediumblue;
+                border: 1px solid blue;
+                color: white;
+            }
+
+        .btn-secondary {
+            background-color: white;
+            border: 1px solid blue;
+            color: black;
+        }
+
+            .btn-secondary:hover {
+                background-color: whitesmoke;
+                border: 1px solid blue;
+                color: black;
+            }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="text-center">Employee Registration</h1>
+            <div class="form-control">
+                <div class="form-control-left">
+                    <div class="form-group">
+                        <asp:Label ID="Label1" runat="server" Text="Full Name"></asp:Label>
+                        <asp:TextBox ID="TextBox1" CssClass="input-text" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label2" runat="server" Text="Date of Birth"></asp:Label>
+                        <asp:TextBox ID="TextBox2" CssClass="input-text" TextMode="Date" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label9" runat="server" Text="Country"></asp:Label>
+                        <asp:DropDownList ID="ddlCountries" DataTextField="country_name" AutoPostBack="true" DataValueField="country_id" OnSelectedIndexChanged="ddlCountries_SelectedIndexChanged" CssClass="input-text" runat="server"></asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label11" runat="server" Text="City"></asp:Label>
+                        <asp:DropDownList ID="ddlCities" CssClass="input-text" runat="server">
+                        </asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label7" runat="server" Text="Pincode"></asp:Label>
+                        <asp:TextBox ID="tb_Pincode" CssClass="input-text" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label5" runat="server" Text="Gender"></asp:Label>
+                        <div>
+                            <asp:RadioButtonList ID="rblGender" runat="server">
+                            </asp:RadioButtonList>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-control-right">
+                    <div class="form-group">
+                        <asp:Label ID="Label3" runat="server" Text="Email"></asp:Label>
+                        <asp:TextBox ID="TextBox3" CssClass="input-text" TextMode="Email" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label4" runat="server" Text="Mobile"></asp:Label>
+                        <asp:TextBox ID="TextBox4" CssClass="input-text" TextMode="Phone" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label10" runat="server" Text="State"></asp:Label>
+                        <asp:DropDownList ID="ddlStates" DataTextField="state_name" AutoPostBack="true" DataValueField="state_id" OnSelectedIndexChanged="ddlStates_SelectedIndexChanged" CssClass="input-text" runat="server">
+                        </asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label6" runat="server" Text="Address"></asp:Label>
+                        <asp:TextBox ID="tb_Address" CssClass="input-text" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label12" runat="server" Text="Job Type"></asp:Label>
+                        <asp:DropDownList ID="ddlJobType" CssClass="input-text" runat="server">
+                        </asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label ID="Label13" runat="server" Text="Employee Type"></asp:Label>
+                        <asp:DropDownList ID="ddlEmployeeType" CssClass="input-text" runat="server">
+                        </asp:DropDownList>
+                    </div>
+                </div>
+            </div>
+            <div class="btn-group">
+                <asp:Button CssClass="btn btn-primary" ID="Submit" runat="server" Text="Submit" />
+                <asp:Button CssClass="btn btn-secondary" ID="Cancel" runat="server" Text="Cancel" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee_Registration
+{
+    public partial class Register : Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["AmrutDBConnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCountries();
+                BindJobType();
+                BindEmployeeType();
+                BindGender();
+            }
+        }
+
+        private void BindCountries()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetCountry_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        ddlCountries.DataSource = dt;
+                        ddlCountries.DataTextField = "country_name"; // Column name for display text
+                        ddlCountries.DataValueField = "country_id";  // Column name for value
+                        ddlCountries.DataBind();
+
+                        // Optionally add a default item
+                        ddlCountries.Items.Insert(0, new ListItem("Select Country", ""));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void BindStates(int country_id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetState_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@country_id", country_id);
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        ddlStates.DataSource = dt;
+                        ddlStates.DataTextField = "state_name";
+                        ddlStates.DataValueField = "state_id";
+                        ddlStates.DataBind();
+
+                        // Optionally add a default item
+                        ddlStates.Items.Insert(0, new ListItem("Select State", "0"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void BindCities(int state_id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetCity_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue ("@state_id", state_id);
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        ddlCities.DataSource = dt;
+                        ddlCities.DataTextField = "city_name";
+                        ddlCities.DataValueField = "city_id";
+                        ddlCities.DataBind();
+
+                        // Optionally add a default item
+                        ddlCities.Items.Insert(0, new ListItem("Select City", "0"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void BindJobType()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetJobType_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        ddlJobType.DataSource = dt;
+                        ddlJobType.DataTextField = "lookup_name";
+                        ddlJobType.DataValueField = "lookup_id";
+                        ddlJobType.DataBind();
+
+                        // Optionally add a default item
+                        ddlJobType.Items.Insert(0, new ListItem("Select Job Type", ""));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void BindEmployeeType()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetEmployeeType_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        ddlEmployeeType.DataSource = dt;
+                        ddlEmployeeType.DataTextField = "lookup_name";
+                        ddlEmployeeType.DataValueField = "lookup_id";
+                        ddlEmployeeType.DataBind();
+
+                        // Optionally add a default item
+                        ddlEmployeeType.Items.Insert(0, new ListItem("Select Employee Type", ""));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void BindGender()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_GetGender_Amrut", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlDataAdapter ds = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        ds.Fill(dt);
+
+                        rblGender.DataSource = dt;
+                        rblGender.DataTextField = "lookup_name";
+                        rblGender.DataValueField = "lookup_id";
+                        rblGender.DataBind();
+
+                        // Optionally add a default item
+                        //rblGender.Items.Insert(0, new ListItem("Select Job Type", ""));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the detailed error
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        protected void ddlCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ddlCountries.SelectedIndex != 0)
+                {
+                    int country_id = int.Parse(ddlCountries.SelectedValue);
+                    BindStates(country_id);
+                }
+                else
+                {
+                    BindStates(0);
+                    BindCities(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        protected void ddlStates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ddlStates.SelectedIndex != 0)
+                {
+                    int state_id = int.Parse(ddlStates.SelectedValue);
+                    BindCities(state_id);
+                }
+                else
+                {
+                    BindCities(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
